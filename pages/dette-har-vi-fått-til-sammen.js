f3ad9fd3c80532731645957245d6c85e1810598d
Yvonne H <yvonnehelland@outlook.com>
2021-04-16 17:38:57 +0200

    Creates new page

---
+++ pages/dette-har-vi-fått-til-sammen.js
@@ -0,0 +1,36 @@
+import Head from "next/head";
+
+const Sammen = () => {
+  return (
+    <>
+      <Head>
+        <title>Helping Hands - Dette har vi fått til sammen</title>
+        <link rel="icon" href="/helping-hands-logo-icon.svg" />
+      </Head>
+
+      <div className="historie1">
+        <h2>Dette har vi fått til sammen</h2>
+        <h3>Historie 1</h3>
+        <p>Tekst her</p>
+      </div>
+      <div className="historie2">
+        <h3>Historie 2</h3>
+        <p>Tekst her</p>
+      </div>
+      <div className="historie3">
+        <h3>Historie 3</h3>
+        <p>Tekst her</p>
+      </div>
+      <div className="historie4">
+        <h3>Historie 4</h3>
+        <p>Tekst her</p>
+      </div>
+      <div className="historie5">
+        <h3>Historie 5</h3>
+        <p>Tekst her</p>
+      </div>
+    </>
+  );
+};
+
+export default Sammen;
