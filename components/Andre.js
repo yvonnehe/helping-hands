3f6f3159136a4a218fa34ae594a577bfe03b1f4a
Yvonne H <yvonnehelland@outlook.com>
2021-04-18 17:13:12 +0200

    Update adds names

--- components/Andre.js
+++ components/Andre.js
@@ -1,10 +1,24 @@
-const Andre = ({ setImg }) => {
+const Andre = ({ setImg, setName, setDesc }) => {
   return (
     <div className="img-grid">
-      <div className="img-wrap" onClick={() => setImg("/mama-vanessa.jpg")}>
+      <div
+        className="img-wrap"
+        onClick={() => {
+          setImg("/mama-vanessa.jpg");
+          setName("Mama Vanessa");
+          setDesc("mama v");
+        }}
+      >
         <img src="/mama-vanessa.jpg"></img>
       </div>
-      <div className="img-wrap" onClick={() => setImg("/ndelekwa.jpg")}>
+      <div
+        className="img-wrap"
+        onClick={() => {
+          setImg("/ndelekwa.jpg");
+          setName("Ndelekwa");
+          setDesc("desc");
+        }}
+      >
         <img src="/ndelekwa.jpg"></img>
       </div>
     </div>
--- components/Fadderbarn.js
+++ components/Fadderbarn.js
@@ -16,59 +16,102 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
       </div>
       <div
         className="img-wrap"
-        onClick={() => setImg("/helping-hands-nathalya.jpg")}
+        onClick={() => {
+          setImg("/helping-hands-nathalya.jpg");
+          setName("Nathalya");
+          setDesc("desc nat");
+        }}
       >
         <img src="/helping-hands-nathalya.jpg" alt="Tanzanian student"></img>
       </div>
       <div
         className="img-wrap"
-        onClick={() => setImg("/helping-hands-obedi.jpg")}
+        onClick={() => {
+          setImg("/helping-hands-obedi.jpg");
+          setName("Obedi");
+          setDesc("obedi");
+        }}
       >
         <img src="/helping-hands-obedi.jpg" alt="Tanzanian student"></img>
       </div>
       <div
         className="img-wrap"
-        onClick={() => setImg("/helping-hands-amos.jpg")}
+        onClick={() => {
+          setImg("/helping-hands-amos.jpg");
+          setName("Amos");
+          setDesc("amos");
+        }}
       >
         <img src="/helping-hands-amos.jpg" alt="Tanzanian student"></img>
       </div>
       <div
         className="img-wrap"
-        onClick={() => setImg("/helping-hands-kurwa.jpg")}
+        onClick={() => {
+          setImg("/helping-hands-kurwa.jpg");
+          setName("Kurwa");
+          setDesc("kurwa");
+        }}
       >
         <img src="/helping-hands-kurwa.jpg" alt="Tanzanian student"></img>
       </div>
       <div
         className="img-wrap"
-        onClick={() => setImg("/helping-hands-musa.jpg")}
+        onClick={() => {
+          setImg("/helping-hands-musa.jpg");
+          setName("Musa");
+          setDesc("musa");
+        }}
       >
         <img src="/helping-hands-musa.jpg" alt="Tanzanian student"></img>
       </div>
       <div
         className="img-wrap"
-        onClick={() => setImg("/helping-hands-joyce.jpg")}
+        onClick={() => {
+          setImg("/helping-hands-joyce.jpg");
+          setName("Joyce");
+          setDesc("joyce");
+        }}
       >
         <img src="/helping-hands-joyce.jpg" alt="Tanzanian student"></img>
       </div>
       <div
         className="img-wrap"
-        onClick={() => setImg("/helping-hands-joseph.jpg")}
+        onClick={() => {
+          setImg("/helping-hands-joseph.jpg");
+          setName("Joseph");
+          setDesc("joseph");
+        }}
       >
         <img src="/helping-hands-joseph.jpg" alt="Tanzanian student"></img>
       </div>
       <div
         className="img-wrap"
-        onClick={() => setImg("/helping-hands-jonas.jpg")}
+        onClick={() => {
+          setImg("/helping-hands-jonas.jpg");
+          setName("Jonas");
+          setDesc("jonas");
+        }}
       >
         <img src="/helping-hands-jonas.jpg" alt="Tanzanian student"></img>
       </div>
       <div
         className="img-wrap"
-        onClick={() => setImg("/helping-hands-ali.jpg")}
+        onClick={() => {
+          setImg("/helping-hands-ali.jpg");
+          setName("Ali");
+          setDesc("desc ali");
+        }}
       >
         <img src="/helping-hands-ali.jpg" alt="Tanzanian student"></img>
       </div>
-      <div className="img-wrap" onClick={() => setImg("/Adrian.jpg")}>
+      <div
+        className="img-wrap"
+        onClick={() => {
+          setImg("/Adrian.jpg");
+          setName("Adrian");
+          setDesc("desc adrian");
+        }}
+      >
         <img src="/Adrian.jpg" alt="Tanzanian child"></img>
       </div>
     </div>
