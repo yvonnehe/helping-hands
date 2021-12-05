26c5e58ab0be639cf96420015042578f1d69369b
yvonnehe <yvonnehelland@outlook.com>
2021-12-05 20:08:02 +0100

    adds zakayo

--- components/Fadderbarn.js
+++ components/Fadderbarn.js
@@ -64,6 +64,17 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
       >
         <img src="/helping-hands-dorcas.jpg" alt="Tanzanian student"></img>
       </div>
+      <div
+        className="img-wrap"
+        onClick={() => {
+          setImg("/helping-hands-zakayo.jpg");
+          setName("Zakayo");
+          setDesc("Zakayo er 20 år gammel. Han og brødrene har mistet begge foreldrene sine. Likevel står de på, og kjemper for drømmene sine. Zakayo er ferdig på videregående med god hjelp fra faddere. Nå har han begynt på studie for å bli farmasøyt. For skolegang, kost og losji, pensumbøker og alt utstyr han trenger koster det 12 600 kr i året. Han har allerede en fadder, men trenger flere som kan støtte med den resterende summen på 740 kr i mnd."
+          );
+        }}
+      >
+        <img src="/helping-hands-zakayo.jpg" alt="Tanzanian student"></img>
+      </div>
     </div>
   );
 };
