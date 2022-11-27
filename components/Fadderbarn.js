38605241483a997163aeb0b998d0b479d84bd303
yvonnehe <yvonnehelland@outlook.com>
2022-11-27 13:14:03 +0100

    removes dorcas

--- components/Fadderbarn.js
+++ components/Fadderbarn.js
@@ -42,18 +42,6 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
       <div
         className="img-wrap"
         onClick={() => {
-          setImg("/helping-hands-dorcas.jpg");
-          setName("Dorcas");
-          setDesc(
-            "Moren til Dorcas fikk store økonomiske problemer da mannen døde. Dorcas trenger hjelp til skolegangen. Hun har allerede to faddere, men trenger flere for å dekke alle utgiftene. Du kan støtte henne med skolegang, uniform, sekk og skoleutstyr for de resterende 240 kr i mnd."
-          );
-        }}
-      >
-        <img src="/helping-hands-dorcas.jpg" alt="Tanzanian student"></img>
-      </div>
-      <div
-        className="img-wrap"
-        onClick={() => {
           setImg("/helping-hands-zakayo.jpg");
           setName("Zakayo");
           setDesc("Zakayo er 20 år gammel. Han og brødrene har mistet begge foreldrene sine. Likevel står de på, og kjemper for drømmene sine. Zakayo er ferdig på videregående med god hjelp fra faddere. Nå har han begynt på studie for å bli farmasøyt. For skolegang, kost og losji, pensumbøker og alt utstyr han trenger koster det 12 600 kr i året. Han har allerede en fadder, men trenger flere som kan støtte med den resterende summen på 740 kr i mnd."
