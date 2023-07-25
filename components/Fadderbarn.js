ccb1929f36ddc181901645843a349a754e8eb773
yvonnehe <yvonnehelland@outlook.com>
2023-07-25 12:08:15 +0300

    students images fix

--- components/Fadderbarn.js
+++ components/Fadderbarn.js
@@ -26,68 +26,68 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
       <div
         className="img-wrap"
         onClick={() => {
-          setImg("/helping-hands-rashid.png");
+          setImg("/helping-hands-rashid.jpg");
           setName("Rashid");
           setDesc("Han er 12 år gammel og går siste året på barneskolen. Han har 5 søsken. Han trenger hjelp til videre skolegang. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 100 kr i mnd."
           );
         }}
       >
-        <img src="/helping-hands-rashid.png" alt="Tanzaniansk student"></img>
+        <img src="/helping-hands-rashid.jpg" alt="Tanzaniansk student"></img>
       </div>
       <div
         className="img-wrap"
         onClick={() => {
-          setImg("/helping-hands-mary.png");
+          setImg("/helping-hands-mary.jpg");
           setName("Mary");
           setDesc("Hun er 13 år gammel og går siste året på barneskolen. Hun bor med mor og 2 søsken. Hun trenger din hjelp. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 100 kr i mnd."
           );
         }}
       >
-        <img src="/helping-hands-mary.png" alt="Tanzaniansk student"></img>
+        <img src="/helping-hands-mary.jpg" alt="Tanzaniansk student"></img>
       </div>
       <div
         className="img-wrap"
         onClick={() => {
-          setImg("/helping-hands-abdala.png");
+          setImg("/helping-hands-abdala.jpg");
           setName("Abdala");
           setDesc("Han er 9 år gammel og går i 3. Klasse. Han bor med mor og lillebror. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 100 kr i mnd."
           );
         }}
       >
-        <img src="/helping-hands-abdala.png" alt="Tanzaniansk student"></img>
+        <img src="/helping-hands-abdala.jpg" alt="Tanzaniansk student"></img>
       </div>
       <div
         className="img-wrap"
         onClick={() => {
-          setImg("/helping-hands-christopher.png");
+          setImg("/helping-hands-christopher.jpg");
           setName("Christopher");
           setDesc("Han er 8 år gammel og går i 3.klasse. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 100 kr i mnd."
           );
         }}
       >
-        <img src="/helping-hands-christopher.png" alt="Tanzaniansk student"></img>
+        <img src="/helping-hands-christopher.jpg" alt="Tanzaniansk student"></img>
       </div>
       <div
         className="img-wrap"
         onClick={() => {
-          setImg("/helping-hands-salimu.png");
+          setImg("/helping-hands-salimu.jpg");
           setName("Salimu");
           setDesc("Han er 11 år gammel og går i 5.klasse. Han bor med mor og 2 søsken. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 100 kr i mnd."
           );
         }}
       >
-        <img src="/helping-hands-salimu.png" alt="Tanzaniansk student"></img>
+        <img src="/helping-hands-salimu.jpg" alt="Tanzaniansk student"></img>
       </div>
       <div
         className="img-wrap"
         onClick={() => {
-          setImg("/helping-hands-irene.png");
+          setImg("/helping-hands-irene.jpg");
           setName("Irene");
           setDesc("Hun er 17 år gammel. Hun vil gjerne fortsette skolegangen, men moren har andre planer og vil ikke betale for henne. Hun trenger hjelp til å betale ungdomsskolen. For 480kr i mnd kan du hjelpe henne med skolegang, uniform og alle skolesaker hun trenger."
           );
         }}
       >
-        <img src="/helping-hands-irene.png" alt="Tanzaniansk student"></img>
+        <img src="/helping-hands-irene.jpg" alt="Tanzaniansk student"></img>
       </div>
     </div>
   );
