41d0aba907f31cb9889a23e2e3fbde0200e08e53
yvonnehe <yvonnehelland@outlook.com>
2023-09-06 16:10:10 +0200

    fonts again

--- components/NextHead.js
+++ components/NextHead.js
@@ -5,6 +5,7 @@ const NextHead = () => {
     <Head>
       <title>Helping Hands Norway</title>
       <link rel="icon" href="/helping-hands-logo-icon.svg" />
+      <link rel="stylesheet" href="https://use.typekit.net/krv6awl.css"></link>
       <meta
         name="description"
         content="Helping Hands er en veldedig organisasjon som jobber med å få barn og ungdom i skolegang i Tanzania."
