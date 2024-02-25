893f071810cb6f11daeab127a5c76ecabc7358c1
yvonnehe <yvonnehelland@outlook.com>
2024-02-25 20:39:22 +0100

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Layout from "../components/layout/Layout";
+import { Analytics } from '@vercel/analytics/react';
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/main.scss";
 
@@ -6,6 +7,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
