b83fdd2f15726a36b5bcb5ad7bcff08aa423c1c2
yvonnehe <yvonnehelland@outlook.com>
2024-03-08 15:11:13 +0100

    accessibility for close button

--- components/MerInfo.js
+++ components/MerInfo.js
@@ -1,14 +1,34 @@
+import { useEffect, useRef } from 'react';
+
 const MerInfo = ({ Img, setImg, Name, setName, Desc, setDesc }) => {
+  const closeBtnRef = useRef(null);
+
   const exit = (e) => {
     if (e.target.classList.contains("backdrop") || e.currentTarget.className === "close-btn") {
       setImg(null);
     }
   };
 
+  useEffect(() => {
+    const handleKeyUp = (e) => {
+      if (e.key === 'Escape') {
+        setImg(null);
+      }
+    };
+
+    window.addEventListener('keyup', handleKeyUp);
+    // Auto-focus on the close button when the popup is opened
+    closeBtnRef.current.focus();
+
+    return () => {
+      window.removeEventListener('keyup', handleKeyUp);
+    };
+  }, []);
+
   return (
     <div className="backdrop" onClick={exit}>
       <div className="description">
-        <button onClick={exit} class="close-btn">&times;</button>
+        <button onClick={exit} class="close-btn" tabIndex="0" ref={closeBtnRef}>&times;</button>
         <img src={Img} alt="Bigger image"></img>
         <h4 className="name">{Name}</h4>
         <p className="desc">{Desc}</p>
