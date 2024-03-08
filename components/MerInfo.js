13fd72551b2da1e14b0a073498be742065beb989
yvonnehe <yvonnehelland@outlook.com>
2024-03-08 15:04:19 +0100

    adds close button

--- components/MerInfo.js
+++ components/MerInfo.js
@@ -1,6 +1,6 @@
 const MerInfo = ({ Img, setImg, Name, setName, Desc, setDesc }) => {
   const exit = (e) => {
-    if (e.target.classList.contains("backdrop")) {
+    if (e.target.classList.contains("backdrop") || e.currentTarget.className === "close-btn") {
       setImg(null);
     }
   };
@@ -8,6 +8,7 @@ const MerInfo = ({ Img, setImg, Name, setName, Desc, setDesc }) => {
   return (
     <div className="backdrop" onClick={exit}>
       <div className="description">
+        <button onClick={exit} class="close-btn">&times;</button>
         <img src={Img} alt="Bigger image"></img>
         <h4 className="name">{Name}</h4>
         <p className="desc">{Desc}</p>
