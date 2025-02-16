31a4878708d23c4bfdf193cadfd373634659d8f2
yvonnehe <yvonnehelland@outlook.com>
2025-02-16 01:09:30 +0100

    continued - adds direct button from children

--- components/Fadderbarn.js
+++ components/Fadderbarn.js
@@ -1,4 +1,4 @@
-const Fadderbarn = ({ setImg, setName, setDesc }) => {
+const Fadderbarn = ({ setImg, setName, setDesc, setAmount }) => {
   return (
     <div className="img-grid">
       <button
@@ -9,6 +9,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Mary");
           setDesc("Hun er 14 år gammel og går i 8. klasse. Hun bor med mor og 2 søsken. Hun trenger din hjelp. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 150 kr i mnd."
           );
+          setAmount(150);
         }}
       >
         <img src="/fadderbarn/helping-hands-mary.jpg" alt="Tanzaniansk student"></img>
@@ -21,6 +22,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Abdala");
           setDesc("Han er 10 år gammel og går i 4. Klasse. Han bor med mor og lillebror. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 100 kr i mnd."
           );
+          setAmount(100);
         }}
       >
         <img src="/fadderbarn/helping-hands-abdala.jpg" alt="Tanzaniansk student"></img>
@@ -33,6 +35,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Salimu");
           setDesc("Han er 12 år gammel og går i 6. klasse. Han bor med mor og 2 søsken. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 100 kr i mnd."
           );
+          setAmount(100);
         }}
       >
         <img src="/fadderbarn/helping-hands-salimu.jpg" alt="Tanzaniansk student"></img>
@@ -45,6 +48,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Mahad");
           setDesc("Han er 13 år og går i 6. klasse. Han bor med besteforeldrene, moren og 3 søsken. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 100 kr i mnd."
           );
+          setAmount(100);
         }}
       >
         <img src="/fadderbarn/helping-hands-mahad.jpg" alt="Tanzaniansk student"></img>
@@ -57,6 +61,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Ali");
           setDesc("Han er 9 år og går i 2. klasse. Han bor med mor og fire søsken. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 100 kr i mnd."
           );
+          setAmount(100);
         }}
       >
         <img src="/fadderbarn/helping-hands-ali.jpg" alt="Tanzaniansk student"></img>
@@ -69,6 +74,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Esther");
           setDesc("Hun er 10 år og går i 3. klasse. Hun bor med mor og 2 søstre. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 100 kr i mnd."
           );
+          setAmount(100);
         }}
       >
         <img src="/fadderbarn/helping-hands-esther.jpg" alt="Tanzaniansk student"></img>
@@ -81,6 +87,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Yahaya");
           setDesc("Han er 10 år gammel og går i 4. klasse. Han bor med foreldrene og har 4 søsken. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 100 kr i mnd."
           );
+          setAmount(100);
         }}
       >
         <img src="/fadderbarn/helping-hands-yahaya.jpg" alt="Tanzaniansk student"></img>
@@ -93,6 +100,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Kelvin");
           setDesc("Han er 13 år og går i 7.klasse. Han bor med foreldrene og 3 søsken. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 100 kr i mnd."
           );
+          setAmount(100);
         }}
       >
         <img src="/fadderbarn/helping-hands-kelvin.jpg" alt="Tanzaniansk student"></img>
@@ -105,6 +113,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Nordin");
           setDesc("Han er 10 år og går i 3. klasse. Han bor med foreldrene og 4 søsken. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 100 kr i mnd."
           );
+          setAmount(100);
         }}
       >
         <img src="/fadderbarn/helping-hands-nordin.jpg" alt="Tanzaniansk student"></img>
@@ -117,6 +126,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Sofia");
           setDesc("Hun er 15 år gammel og går i 9. klasse. Hun bor med mor og ett søsken. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 150 kr i mnd."
           );
+          setAmount(150);
         }}
       >
         <img src="/fadderbarn/helping-hands-sofia.jpg" alt="Tanzaniansk student"></img>
@@ -129,6 +139,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Abdul");
           setDesc("Han er 16 år og går i 9. klasse. Han bor med besteforeldre, mor og 3 søsken. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 150 kr i mnd."
           );
+          setAmount(150);
         }}
       >
         <img src="/fadderbarn/helping-hands-abdul.jpg" alt="Tanzaniansk student"></img>
@@ -141,6 +152,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Bahati");
           setDesc("Hun er 20 år gammel. Hun har akkurat begynt på studiet “Social development studies”, og drømmen hennes er å jobbe med barn som har spesielle behov. Hun bor i en stor familie med mange søsken og «søsken». Gjenstående skoleutgifter, uniform og alle andre ting hun trenger i forbindelse med skolen, er 500 kr i mnd. Denne utgiften kan helt fint deles med én eller flere faddere som du enten kjenner eller ikke kjenner."
           );
+          setAmount(500);
         }}
       >
         <img src="/fadderbarn/helping-hands-bahati.jpg" alt="Tanzaniansk student"></img>
@@ -153,6 +165,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Halima");
           setDesc("Hun er 16 år gammel og går på studiet “hotel management”. Hun bor med foreldre og 4 søsken. Hun har allerede én fadder, men trenger én eller flere til, for å dekke de resterende 480 kr i mnd. Dette dekker skoleutgifter, uniform og alle andre ting hun trenger i forbindelse med skolen."
           );
+          setAmount(480);
         }}
       >
         <img src="/fadderbarn/helping-hands-halima.jpg" alt="Tanzaniansk student"></img>
@@ -165,6 +178,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Jonelle");
           setDesc("Hun er 4 år og går på “pre school”. Hun bor med 3 andre barn/unge voksne fra slekten hennes. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 100 kr i mnd."
           );
+          setAmount(100);
         }}
       >
         <img src="/fadderbarn/helping-hands-jonelle.jpg" alt="Tanzaniansk student"></img>
@@ -177,6 +191,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Jasin");
           setDesc("Hun er 15 år og går i 9. klasse. Hun bor med storesøster og to yngre barn fra slekten deres. Hun trenger hjelp til videre skolegang. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 150 kr i mnd."
           );
+          setAmount(150);
         }}
       >
         <img src="/fadderbarn/helping-hands-jasin.jpg" alt="Tanzaniansk student"></img>
@@ -189,6 +204,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Norin");
           setDesc("Hun er 12 år og går i 5. klasse. Hun bor med 3 andre barn og unge voksne fra slekten hennes. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 100 kr i mnd."
           );
+          setAmount(100);
         }}
       >
         <img src="/fadderbarn/helping-hands-norin.jpg" alt="Tanzaniansk student"></img>
@@ -201,6 +217,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Rashid");
           setDesc("Han er 13 år gammel og går i 8. klasse. Han har 5 søsken. Han trenger hjelp til videre skolegang. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 150 kr i mnd."
           );
+          setAmount(150);
         }}
       >
         <img src="/fadderbarn/helping-hands-rashid.jpg" alt="Tanzaniansk student"></img>
@@ -213,6 +230,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Ramla");
           setDesc("Hun er 21 år gammel og drømmer om å studere “hotel management”. Hun bor sammen med bestemoren og 4 andre barn/ungdom. For skolepenger, uniform, kost, losji, hygieneprodukter og skolesaker vil det koste 680 kr i mnd. Denne summen kan fint deles på flere faddere."
           );
+          setAmount(680);
         }}
       >
         <img src="/fadderbarn/helping-hands-ramla.jpg" alt="Tanzaniansk student"></img>
@@ -225,6 +243,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Hadija");
           setDesc("Hun er 11 år og går i 5.klasse. Hun bor med mor og 2 søsken. Hun har allerede en fadder, men trenger en til. For de resterende 50 kr støtter du henne med mat på skolen, sko, sekk, uniform og andre nødvendigheter."
           );
+          setAmount(50);
         }}
       >
         <img src="/fadderbarn/helping-hands-hadija.jpg" alt="Tanzaniansk student"></img>
@@ -237,6 +256,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Jackson");
           setDesc("Han er 11 år og går i 5.klasse. Han bor med fetter, kusine og besteforeldre. Han har allerede en fadder, men trenger en til. For de resterende 50 kr støtter du ham med mat på skolen, sko, sekk, uniform og andre nødvendigheter."
           );
+          setAmount(50);
         }}
       >
         <img src="/fadderbarn/helping-hands-jackson.jpg" alt="Tanzaniansk student"></img>
@@ -249,6 +269,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Ali");
           setDesc("Han er 12 år gammel og går i 5. klasse. Han bor med mor og mange søsken. Han har allerede en fadder, men trenger en til. For de resterende 50 kr støtter du ham med mat på skolen, sko, sekk, uniform og andre nødvendigheter."
           );
+          setAmount(50);
         }}
       >
         <img src="/fadderbarn/helping-hands-ali2.jpg" alt="Tanzaniansk student"></img>
@@ -261,6 +282,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Augusti");
           setDesc("Han er 15 år og skal snart begynne på ungdomsskolen. Han bor med bestemor og søsken/søskenbarn. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 150 kr i mnd."
           );
+          setAmount(150);
         }}
       >
         <img src="/fadderbarn/helping-hands-augusti.jpg" alt="Tanzaniansk student"></img>
@@ -273,6 +295,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Alexi");
           setDesc("Han er 11 år gammel og går i 4. klasse. Han bor med mor og 3 brødre. Han har allerede en fadder, men trenger en til. For de resterende 50 kr støtter du han med mat på skolen, sko, sekk, uniform og andre nødvendigheter."
           );
+          setAmount(50);
         }}
       >
         <img src="/fadderbarn/helping-hands-alexi.jpg" alt="Tanzaniansk student"></img>
@@ -285,6 +308,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Omar");
           setDesc("Han er 14 år og skal snart begynne på ungdomsskolen. Han bor med foreldrene og storebror. Han har allerede en fadder, men trenger en til. For de resterende 100 kr støtter du ham med mat på skolen, sko, sekk, uniform og andre nødvendigheter."
           );
+          setAmount(100);
         }}
       >
         <img src="/fadderbarn/helping-hands-omar.jpg" alt="Tanzaniansk student"></img>
@@ -297,6 +321,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Jamal");
           setDesc("Han er 16 år og går i 9.klasse. Han bor med foreldrene og lillebror. Han har allerede en fadder, men trenger en til. For de resterende 100 kr støtter du han med mat på skolen, sko, sekk, uniform og andre nødvendigheter."
           );
+          setAmount(100);
         }}
       >
         <img src="/fadderbarn/helping-hands-jamal.jpg" alt="Tanzaniansk student"></img>
@@ -309,6 +334,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Ramadhan");
           setDesc("Han er 16 år gammel og går i 8. klasse. Han bor med mor og flere søsken. Han har allerede en fadder, men trenger en til. For de resterende 100 kr støtter du ham med mat på skolen, sko, sekk, uniform og andre nødvendigheter."
           );
+          setAmount(100);
         }}
       >
         <img src="/fadderbarn/helping-hands-ramadhan.jpg" alt="Tanzaniansk student"></img>
@@ -321,6 +347,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Halima");
           setDesc("Hun er 15 år og går i 10.klasse. Hun bor i en stor familie, med mange søsken, søskenbarn, og flere mødre. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 150 kr i mnd."
           );
+          setAmount(150);
         }}
       >
         <img src="/fadderbarn/helping-hands-halima2.jpg" alt="Tanzaniansk student"></img>
@@ -333,6 +360,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Marthina");
           setDesc("Hun er 17 år gammel og går i 10. klasse. Hun bor med mor og 3 søsken. Hun har allerede en fadder, men trenger en til. For de resterende 100 kr støtter du henne med mat på skolen, sko, sekk, uniform og andre nødvendigheter."
           );
+          setAmount(100);
         }}
       >
         <img src="/fadderbarn/helping-hands-marthina.jpg" alt="Tanzaniansk student"></img>
@@ -345,6 +373,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Amina");
           setDesc("Hun er 16 år gammel og vil gjerne bli skredder. Hun bor med bestemor og 4 søskenbarn/søsken. For utdannelsen og alt annet hun trenger i forbindelse med skolegangen, vil det koste 650 kr i mnd. Denne summen kan fint deles mellom flere faddere."
           );
+          setAmount(650);
         }}
       >
         <img src="/fadderbarn/helping-hands-amina.jpg" alt="Tanzaniansk student"></img>
@@ -357,6 +386,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Ramadhan");
           setDesc("Han er 20 år gammel. Han har stort behov for hjelpen din. Han vil gjerne begynne på fagskole og bli mekaniker. For skolepenger, kost og losji og alt annet han trenger i forbindelse med skolegangen, vil det koste 700 kr i mnd. Denne summen kan fint deles mellom flere faddere."
           );
+          setAmount(700);
         }}
       >
         <img src="/fadderbarn/helping-hands-ramadhan2.jpg" alt="Tanzaniansk student"></img>
@@ -369,6 +399,7 @@ const Fadderbarn = ({ setImg, setName, setDesc }) => {
           setName("Hawa");
           setDesc("Hun er 15 år gammel og trenger hjelp til videre skolegang. Hun venter på å få begynne på ungdomsskolen. For 150 kr i mnd støtter du henne med mat på skolen, sko, sekk, uniform og andre nødvendigheter."
           );
+          setAmount(150);
         }}
       >
         <img src="/fadderbarn/helping-hands-hawa.jpg" alt="Tanzaniansk student"></img>
