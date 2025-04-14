e24a4e970f04648b5aefdef03600c4672167a42f
yvonnehe <yvonnehelland@outlook.com>
2025-04-14 17:20:36 +0200

    adds user info to vipps portal

--- pages/api/initiateYearlyPayment.ts
+++ pages/api/initiateYearlyPayment.ts
@@ -58,6 +58,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 amount: amount, // Same as yearly amount
                 description: `Første betaling for ${productName}`,
                 transactionType: "DIRECT_CAPTURE"
+            },
+            userInfo: {
+                scope: "name phoneNumber" // 👈 This requests customer data
             }
         };
 
