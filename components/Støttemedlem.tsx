515ee35700a2bc4a15950a1748a6a77fa6a224ba
yvonnehe <yvonnehelland@outlook.com>
2025-04-17 18:49:45 +0200

    test amount for dev

--- components/Støttemedlem.tsx
+++ components/Støttemedlem.tsx
@@ -27,7 +27,9 @@ const StotteMedlem = () => {
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [errorMessage, setErrorMessage] = useState("");
     const [formattedPhone, setFormattedPhone] = useState("");
-    const amount = 50; // Fixed yearly membership fee
+    //const amount = 50; // Fixed yearly membership fee
+    // TESTING AMOUNT
+    const amount = 5; // Test amount for development
 
     const step2Form = useForm({
         resolver: yupResolver(step2Schema),
