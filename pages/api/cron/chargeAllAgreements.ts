81aeddde0673d44bf6add096de0aafc97570ea5b
yvonnehe <yvonnehelland@outlook.com>
2025-05-31 21:10:44 +0200

    temp comment out auth for cron

--- pages/api/cron/chargeAllAgreements.ts
+++ pages/api/cron/chargeAllAgreements.ts
@@ -10,9 +10,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return res.status(405).json({ error: "Method not allowed" });
     }
 
-    if (req.headers.authorization !== `Bearer ${process.env.CRON_SECRET}`) {
-        return res.status(401).json({ error: "Unauthorized" });
-    }
+    // if (req.headers.authorization !== `Bearer ${process.env.CRON_SECRET}`) {
+    //     return res.status(401).json({ error: "Unauthorized" });
+    // }
 
     try {
         const accessToken = await getVippsAccessToken();
