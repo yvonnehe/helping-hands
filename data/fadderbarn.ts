2cc64361fe05dfaa524e74fc44362e9aedfae134
yvonnehe <yvonnehelland@outlook.com>
2026-06-13 16:36:04 +0200

    fjerner fadderbarn som har fått fadder

--- data/fadderbarn.ts
+++ data/fadderbarn.ts
@@ -8,13 +8,6 @@ export type FadderbarnData = {
 
 export const fadderbarnList: FadderbarnData[] = [
     {
-        name: "Yahaya",
-        image: "/fadderbarn/helping-hands-yahaya.jpg",
-        description: "Han er 11 år gammel og går i 5. klasse. Han bor med foreldrene og har 4 søsken. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 100 kr i mnd.",
-        amount: 100,
-        category: "fadderbarn",
-    },
-    {
         name: "Nordin",
         image: "/fadderbarn/helping-hands-nordin.jpg",
         description: "Han er 10 år og går i 4. klasse. Han bor med foreldrene og 4 søsken. For mat på skolen, sko, sekk, uniform og andre nødvendigheter koster det 100 kr i mnd.",
@@ -26,13 +19,6 @@ export const fadderbarnList: FadderbarnData[] = [
         image: "/fadderbarn/helping-hands-jamal.jpg",
         description: "Han er 10 år gammel. Han bor med moren og storebroren. Han har også 2 storesøstre. Han går i 4. klasse. For 100 kr i mnd støtter du han med mat på skolen, uniform og alt annet han trenger i forbindelse med skolegangen.",
         amount: 100,
-        category: "fadderbarn",
-    },
-    {
-        name: "Dickson",
-        image: "/fadderbarn/helping-hands-dickson.jpg",
-        description: "Han er 18 år gammel og går på internatskole. Han har allerede én fadder, men trenger flere for å dekke alle utgiftene. De resterende 170 kronene i måneden bidrar til å dekke skolepenger, kost og losji, uniform og alt annet han trenger i forbindelse med skolegangen.",
-        amount: 170,
         category: "fadderbarn",
     },
     // {
